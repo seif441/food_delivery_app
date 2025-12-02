4f2ee4b9cc2733a0d6dff105c5c37ddc1aef172a
jana ayman <janaayman915@gmail.com>
2025-12-03 00:29:53 +0200

    serial

---
+++ src/main/java/com/system/food_delivery_app/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+
+package com.fooddeliveryapp.Repository;
+import com.fooddeliveryapp.model.Order;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByCustomerId(Long customerId);
+}
+ 
