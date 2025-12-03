31e890d1631284ff31379510e4f4d4348043edf7
eyad tamer <eyadt141@gmail.com>
2025-12-03 16:05:21 +0200

    adding files

--- src/main/java/com/system/food_delivery_app/controller/MenuController.java
+++ src/main/java/com/system/food_delivery_app/controller/MenuController.java
@@ -1,5 +1,57 @@
 package com.system.food_delivery_app.controller;
 
+import com.deliveryapp.demo.model.Menu;
+import com.deliveryapp.demo.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/menus")
 public class MenuController {
+
+    private final MenuService menuService;
+
+    @Autowired
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
+    @GetMapping
+    public List<Menu> getAllMenus() {
+        return menuService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Menu> getMenuById(@PathVariable Long id) {
+        return menuService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Menu createMenu(@RequestBody Menu menu) {
+        return menuService.save(menu);
+    }
     
+    @PutMapping("/{id}")
+    public ResponseEntity<Menu> updateMenu(@PathVariable Long id, @RequestBody Menu menuDetails) {
+        Menu updatedMenu = menuService.update(id, menuDetails);
+        if (updatedMenu != null) {
+            return ResponseEntity.ok(updatedMenu);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMenu(@PathVariable Long id) {
+        if (menuService.delete(id)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
